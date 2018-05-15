09e4dc0ea9279777cd06cdd0277a09174585d434
ManuNovas <enam_rda@yahoo.com.mx>
2018-05-15 13:07:43 -0500

    Asistente de Yama para subida de imágenes

---
+++ AsistenteYama.py
@@ -0,0 +1,83 @@
+import mysql.connector
+import io
+
+class AsistenteYama:
+	conexion = None
+
+	def abre_conexion(self, usuario, password, servidor, base_datos):
+		self.conexion = mysql.connector.connect(user = usuario, password = password, host = servidor, database = base_datos)
+
+	def genera_imagenes_galeria(self, entrada):
+		entrada = open('C:\\Users\\Franco\\Documents\\Documentación\\Yama\\' + archivo_entrada, 'r', encoding = 'UTF-8')
+		salida = open('C:\\Users\\Franco\\Documents\\Documentación\\Yama\\imagenes_yama.sql', "w", encoding = 'UTF-8')
+		cursor = self.conexion.cursor( )
+
+		i = 0
+		for linea in entrada.readlines( ):
+			if i != 0:
+				campos = linea.split(',')
+				anio = campos[0]
+				mes = campos[1]
+				imagen = campos[2]
+
+				meses = {
+					'Enero': 1,
+					'Febrero': 1,
+					'Marzo': 1,
+					'Abril': 2,
+					'Mayo': 2,
+					'Junio': 2,
+					'Julio': 3,
+					'Agosto': 3,
+					'Septiembre': 3,
+					'Octubre': 4,
+					'Noviembre': 4,
+					'Diciembre': 4
+				}
+
+				periodos = {
+					1: 'ENERO - MARZO',
+					2: 'ABRIL - JUNIO',
+					3: 'JULIO - SEPTIEMBRE',
+					4: 'OCTUBRE - DICIEMBRE'
+
+				}
+
+				consulta = "SELECT GALERIA_AVANCE_ID FROM GALERIA_AVANCE WHERE MICROSITIO_FK = 1 AND PERIODO_TRIMESTRAL_FK = " + str(meses[mes]) + " AND AÑO = " + anio
+				cursor.execute(consulta)
+				galeria_avance_id = cursor.fetchone( )
+				if galeria_avance_id is None:
+					print('No existe la galeria de ' + mes +  '/' + anio + ' en base')
+					pass
+				else:
+					galeria_avance_id = galeria_avance_id[0]
+					consulta = "INSERT INTO IMAGEN_GALERIA(GALERIA_AVANCE_FK, ENLACE, STATUS) VALUES(" + str(galeria_avance_id) + ", '" + anio + "/" + str(periodos[meses[mes]]) + "/" + mes.upper( ) + "/" +  imagen + "', 1);\n"
+					salida.write(consulta)
+					pass
+
+				pass
+			pass
+			i += 1
+		pass
+
+		entrada.close( )
+		salida.close( )
+
+asistente = AsistenteYama( )
+
+opcion = input('Selecciona una opcion:\n\t1. Exportar imagenes\n\t')
+	
+usuario = input('Escribe el usuario de la base: ')
+password = input('Escribe el password de ' + usuario + ': ')
+servidor = input('Escribe el servidor de la base: ')
+base_datos = input('Escribe el nombre de la base: ')
+asistente.abre_conexion(usuario, password, servidor, base_datos)
+
+if asistente.conexion:
+	if opcion == '1':
+		archivo_entrada = input('Escribe el nombre del archivo de entrada: ')
+		asistente.genera_imagenes_galeria(archivo_entrada)
+		pass
+	pass
+else:
+	print('Error al conectar con la base')
