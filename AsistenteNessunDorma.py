ffbce608089a82afb6291976d86232e15ef8deb1
ManuNovas <enam_rda@yahoo.com.mx>
2018-06-12 16:42:55 -0500

    Cambios en el asistende de NessunDorma

--- AsistenteNessunDorma.py
+++ AsistenteNessunDorma.py
@@ -17,7 +17,7 @@ class AsistenteNessunDorma:
 			consulta = "INSERT INTO ESTADO(CLAVE, NOMBRE, VENTA_M2, INCREMENTO, ZONA_FK) VALUES('" + campo[0] + "', '', " + str(uniform(0, 167327)) + ", " + campo[1] + ", );"
 			archivo_salida.write(consulta + '\n')
 			pass
-		print 'Consulta generada'
+		print('Consulta generada')
 
 		archivo_entrada.close( )
 		archivo_salida.close( )
@@ -47,24 +47,26 @@ class AsistenteNessunDorma:
 			archivo_salida.write(consulta + '\n')
 			pass
 
-		print 'Consulta generada'
+		print('Consulta generada')
 		archivo_salida.close( )
 
-	def genera_consulta_centros(self, entrada):
-		archivo_entrada = open(entrada, 'r')
+	def genera_consulta_centros(self, nombre):
+		archivo_entrada = open(nombre, 'r')
 		archivo_salida = open('SQL/centros_comerciales_nd.sql', 'w')
 
 		for linea in archivo_entrada.readlines( ):
 			campos = linea.split('|')
 			consulta = campos[0]
-			consulta = consulta.replace("}EFK{", campos[1])
-			consulta = consulta.replace("}MFK{", campos[2])
-			consulta = consulta.replace("}VM2{", campos[4])
-			consulta = consulta.replace("}INC{", campos[5])
-			consulta = consulta.replace("}ASO{", campos[6])
-			consulta = consulta.replace("}NOM{", campos[3])
+			consulta = consulta.replace("}EFK{", campos[2])
+			consulta = consulta.replace("}MFK{", campos[3])
+			consulta = consulta.replace("}VM2{", campos[5])
+			consulta = consulta.replace("}INC{", campos[6])
+			consulta = consulta.replace("}ASO{", campos[7])
+			consulta = consulta.replace("}NOM{", campos[4])
 			archivo_salida.write(consulta + ';\n')
 			pass
+
+		print("consulta generada");
 
 		archivo_entrada.close( )
 		archivo_salida.close( )
@@ -73,7 +75,7 @@ class AsistenteNessunDorma:
 		archivo_entrada = open('SQL/incidencia_municipios.sql', 'r')
 		archivo_salida = open('SQL/actualiza_municipios_nd.sql', 'w')
 
-		for linea in archivo_entrada.readlines( ):
+		for linea in archivo_entrada.readlines( ):	
 			campos = linea.split('|')
 			consulta = campos[0]
 			consulta = consulta.replace("}ID{", campos[1])
@@ -81,7 +83,25 @@ class AsistenteNessunDorma:
 			consulta = consulta.replace("}VM2{", campos[5])
 			consulta = consulta.replace("}INC{", campos[6])
 			consulta = consulta.replace("}NOM{", campos[4])
-			archivo_salida.write(consulta + '\n')
+			archivo_salida.write(consulta + ';\n')
+			pass
+
+		archivo_entrada.close( )
+		archivo_salida.close( )
+
+	def actualiza_estados(self):
+		archivo_entrada = open('SQL/incidencia_estados.sql', 'r')
+		archivo_salida = open('SQL/actualiza_estados_nd.sql', 'w')
+
+		for linea in archivo_entrada.readlines( ):	
+			campos = linea.split('|')
+			consulta = campos[0]
+			consulta = consulta.replace("}VM2{", campos[3])
+			consulta = consulta.replace("}INC{", campos[4])
+			consulta = consulta.replace("}NOC{", campos[5])
+			consulta = consulta.replace("}ZFK{", campos[6])
+			consulta = consulta.replace("}NOM{", campos[2])
+			archivo_salida.write(consulta + ';\n')
 			pass
 
 		archivo_entrada.close( )
@@ -89,8 +109,9 @@ class AsistenteNessunDorma:
 
 asistente = AsistenteNessunDorma( )
 
-opcion = input('Selecciona una opcion:\n\t1. Generar consulta de estados\n\t2. Generar consulta de municipios\n\t3. Generar consulta para simular centros comerciales\n\t4. Obtener consulta para actualizar centros comerciales\n\t5. Actualiza municipios\n')
-	
+opcion = input('Selecciona una opcion:\n\t1. Generar consulta de estados\n\t2. Generar consulta de municipios\n\t3. Generar consulta para simular centros comerciales\n\t4. Obtener consulta para actualizar centros comerciales\n\t5. Actualiza municipios\n\t6. Actualiza estados')
+opcion = int(opcion)
+
 if opcion == 1:
 	nombre = raw_input('Escribe el nombre del archivo de entrada: ')
 	asistente.genera_consulta_estados(nombre)
@@ -103,9 +124,12 @@ elif opcion == 3:
 	asistente.simula_centro_comercial_estados( )
 	pass
 elif opcion == 4:
-	nombre = raw_input('Escribe el nombre del archivo de entrada: ')
+	nombre = input('Escribe el nombre del archivo de entrada: ')
 	asistente.genera_consulta_centros(nombre)
 	pass
 elif opcion == 5:
 	asistente.actualiza_municipios( )
+	pass
+elif opcion == 6:
+	asistente.actualiza_estados( )
 	pass
